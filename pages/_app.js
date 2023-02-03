729689aabe6e30677c6e5f5b2e042bcf89092d0d
chiragpay3 <chiraga@pay3.money>
2023-02-04 02:21:39 +0530

    metadata update

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ export default function App({ Component, pageProps }) {
         <title>Box Protocol</title>
         <meta
           name="description"
-          content="Box Protocol is a DeFi app to make tokenized investment in boxes."
+          content="A DeFi app that lets you invest in tokenized boxes of crypto assets, representing the hottest ideas and sectors in Web3."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/BoxProtocol.ico" />
